9e9e9c7edaf4b6dce4fb7d98b1a4c53b1b69b3a6
varun9449 <99817024+varun9449@users.noreply.github.com>
2022-03-04 01:37:31 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,134 @@
+resource "azurerm_resource_group" "RG_01" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+
+
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.virtual_network_name
+  location            = azurerm_resource_group.RG_01.location
+  resource_group_name = azurerm_resource_group.RG_01.name
+  address_space =     ["10.0.0.0/24"]
+  tags = var.tags
+}
+
+
+resource "azurerm_subnet" "snet" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.RG_01.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.0.0/27"]
+
+  
+}
+
+resource "azurerm_public_ip" "pubip" {
+  name                = var.pubip
+  resource_group_name = azurerm_resource_group.RG_01.name
+  location            = azurerm_resource_group.RG_01.location
+  allocation_method   = "Static"
+
+  tags = var.tags
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.nsg_name
+  location            = azurerm_resource_group.RG_01.location
+  resource_group_name = azurerm_resource_group.RG_01.name
+
+  security_rule {
+    name                       = "RDP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "win"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = 5986
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+security_rule {
+    name                       = "winrm"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = 5985
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+
+  tags = var.tags
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = var.nic_name
+  location            = azurerm_resource_group.RG_01.location
+  resource_group_name = azurerm_resource_group.RG_01.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.snet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.pubip.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nsg-nic" {
+
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+data "azurerm_key_vault" "kv" {
+  name                = "infrakey01"
+  resource_group_name = "RG_01"
+}
+
+
+
+data "azurerm_key_vault_secret" "key" {
+  name         = "infra"
+  key_vault_id = data.azurerm_key_vault.kv.id
+}
+
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.RG_01.name
+  location            = azurerm_resource_group.RG_01.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = data.azurerm_key_vault_secret.key.value
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,70 @@
+variable "resource_group_name" {
+  type        =  string
+  description = "Name of the resource group"
+  default     = "RG_05"
+}
+
+variable "location" {
+  type        =  string
+  description = "Name of the location"
+  default     = "eastus"
+}
+
+variable "tags" {
+  default     = {
+      createdby = "varunteja0706@gmail.com"
+      environment = "development"
+      project = "azure"
+      "department"= "IT"
+  }
+}
+
+variable "virtual_network_name" {
+  type        =  string
+  description = "Name of the vnet"
+  default     = "vnet-01"
+}
+
+variable "subnet_name" {
+  type        =  string
+  description = "Name of the subnet"
+  default     = "sub1"
+}
+
+variable "nsg_name" {
+  type        =  string
+  description = "Name of the subnet"
+  default     = "varnsg"
+}
+
+variable "nic_name" {
+  type        =  string
+  description = "Name of the subnet"
+  default     = "varnic"
+}
+
+variable "vm_name" {
+  type        =  string
+  description = "Name of the subnet"
+  default     = "var-vm-01"
+}
+
+variable "adminusername" {
+  type        =  string
+  description = "Name of the subnet"
+  default     = "azureuser"
+}
+
+variable "adminpassword" {
+  type        =  string
+  description = "Name of the subnet"
+  default     = "Azure@123456"
+}
+
+
+variable "pubip" {
+  type        =  string
+  description = "Name of the subnet"
+  default     = "varpub-01"
+}
+
